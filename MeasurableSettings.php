2d2e2c4ff28fc1c6e99f2d1a541792af9d8bd393
Chardonneaur <contact@ronan-chardonneau.fr>
2022-08-26 19:52:25 +0200

    Update MeasurableSettings.php

--- MeasurableSettings.php
+++ MeasurableSettings.php
@@ -6,38 +6,29 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-namespace Piwik\Plugins\FacebookPageWidgetByAmperage;
+namespace Piwik\Plugins\MastodonWidget;
 
 use Piwik\Settings\Setting;
 use Piwik\Settings\FieldConfig;
 
-/**
- * Defines Settings for FacebookPageWidgetByAmperage.
- *
- * Usage like this:
- * // require Piwik\Plugin\SettingsProvider via Dependency Injection eg in constructor of your class
- * $settings = $settingsProvider->getMeasurableSettings('FacebookPageWidgetByAmperage', $idSite);
- * $settings->appId->getValue();
- * $settings->contactEmails->getValue();
- */
 class MeasurableSettings extends \Piwik\Settings\Measurable\MeasurableSettings
 {
 
     /** @var Setting */
-    public $facebookPageURLSetting;
+    public $mastodonPageURLSetting;
 
     protected function init()
     {
-        $this->facebookPageURLSetting = $this->makeFacebookPageURLSetting();
+        $this->mastodonPageURLSetting = $this->makeMastodonPageURLSetting();
     }
 
-    private function makeFacebookPageURLSetting()
+    private function makeMastodonPageURLSetting()
     {
-        return $this->makeSetting('facebookPageURL', $default = '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
-            $field->title = 'Facebook Page URL';
+        return $this->makeSetting('mastodonPageURL', $default = '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+            $field->title = 'Mastodon Page URL';
             $field->uiControl = FieldConfig::UI_CONTROL_TEXT;
             $field->uiControlAttributes = array('size' => 3);
-            $field->description = 'The URL for your Facebook Page (ex. "https://www.facebook.com/AmperageMarketing/")';
+            $field->description = 'The URL of the Mastodon instance Page (ex. "https://mastodon.floss-marketing-school.com")';
         });
     }
 
